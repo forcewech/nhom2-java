1fbebbcf15d41f71279feecb2ba55c46d692ff82
Nguyen Tien Dung <forcewech@gmail.com>
2023-05-16 02:39:01 +0700

    Giao dien trang chu va trang login

---
+++ src/main/java/com/mycompany/java/nhom2/JAVANHOM2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.java.nhom2;
+
+/**
+ *
+ * @author Administrator
+ */
+public class JAVANHOM2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
